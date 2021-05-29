8e4c8ced4413e18a5b13bfc888a8add125847d59
Maranatha Ilesanmi <maranatha.ilesanmi@gmail.com>
2021-05-29 16:07:18 +0300

    connect fe and api to display default welcome message

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [data, setData] = useState(null);
+
+	useEffect(() => {
+		fetch('/api')
+			.then((res) => res.json())
+			.then((data) => setData(data.message));
+	}, []);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>{!data ? 'Loading...' : data}</p>
+			</header>
+		</div>
+	);
+};
 
 export default App;
